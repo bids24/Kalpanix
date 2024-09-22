f915b30fb6c716d1013d2795219e8eaf2e09dfb2
Gourab Das <dgourab574@gmail.com>
2024-09-23 02:40:17 +0530

    adding main.js

--- components/normal/bg.js
+++ components/normal/bg.js
@@ -1,10 +1,24 @@
 const bgElements = document.querySelectorAll('[class^="bg-"]');
 console.log(bgElements);
-let colors = ["red", "blue", "yellow"];
+
+const colorMapping = {
+    red: 'red',
+    blue: 'blue',
+    yellow: 'yellow',
+};
 
 bgElements.forEach(elem => {
     const colorClass = elem.className.split(' ').find(c => c.startsWith('bg-'));
     console.log(colorClass);
     const color = colorClass ? colorClass.split('-')[1] : 'gray';
-    elem.style.backgroundColor = color;
-})
+    elem.style.backgroundColor = colorMapping[color] || 'gray';
+});
+
+// Export a function to allow reusability
+export function applyBackgroundColors() {
+    bgElements.forEach(elem => {
+        const colorClass = elem.className.split(' ').find(c => c.startsWith('bg-'));
+        const color = colorClass ? colorClass.split('-')[1] : 'gray';
+        elem.style.backgroundColor = colorMapping[color] || 'gray';
+    });
+}
