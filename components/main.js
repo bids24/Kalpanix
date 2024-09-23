ed1a208905047966ca6ccfd5dc57c95d3fd35074
Gourab Das <dgourab574@gmail.com>
2024-09-23 14:15:51 +0530

    change

--- components/main.js
+++ components/main.js
@@ -1,9 +1,6 @@
 import { applyBackgroundColors } from './normal/bg.js';
 import { applyColors } from './normal/colour.js';
-// Call the function to apply background colors
 export function allFunc(){
-    // console.log("HLL");
     applyBackgroundColors();
     applyColors();
 }
-// applyBackgroundColors();
--- components/normal/colour.js
+++ components/normal/colour.js
@@ -1,9 +1,9 @@
-const bgElements = document.querySelectorAll('[class^="colour-"]');
-console.log(bgElements);
+const Elements = document.querySelectorAll('[class^="colour-"]');
+console.log(Elements);
 
 // Export a function to allow reusability
 export function applyColors() {
-    bgElements.forEach(elem => {
+    Elements.forEach(elem => {
         const colorClass = elem.className.split(' ').find(c => c.startsWith('colour-'));
         const color = colorClass ? colorClass.split('-')[1] : 'gray';
         elem.style.color = color;
