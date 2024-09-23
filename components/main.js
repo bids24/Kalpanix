812bb285251033297acd97a03720650f7a5bded1
Gourab Das <dgourab574@gmail.com>
2024-09-23 15:02:34 +0530

    applyFontSizeAndWeight.js

--- components/main.js
+++ components/main.js
@@ -1,6 +1,8 @@
 import { applyBackgroundColors } from './normal/bg.js';
 import { applyColors } from './normal/colour.js';
+import { applyFontSizeAndWeight } from './normal/applyFontSizeAndWeight.js';
 export function allFunc(){
     applyBackgroundColors();
     applyColors();
+    applyFontSizeAndWeight();
 }
