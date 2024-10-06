c6e3c9eadddad7dbdcca6fe17c8bb01c4f69c1a1
subhajit11010 <142614931+subhajit11010@users.noreply.github.com>
2024-10-07 00:29:58 +0530

    13

--- components/elements/dropdown.js
+++ components/elements/dropdown.js
@@ -8,10 +8,12 @@ export function applyDropdown(){
         dropdownClass.children[0].insertAdjacentElement('afterend', invertedCaret);
         dropdownClass.children[1].style.textAlign = "center";
         dropdownClass.children[1].innerHTML = "&#9650;";
-        dropdownClass.children[2].style.textAlign = "right";
+        dropdownClass.children[2].style.marginTop = "1rem";
+        dropdownClass.children[2].style.textAlign = "center";
+        dropdownClass.children[2].style.border = "2px solid #000000";
         dropdownClass.children[2].style.borderRadius = "0.5rem";
         dropdownClass.children[2].style.transition = 'opacity 0.5s ease, height 0.5s ease';
-        dropdownClass.children[2].style.fontSize = "0.8rem";
+        dropdownClass.children[2].style.fontSize = "1rem";
         dropdownClass.children[2].style.listStyleType = 'none';
         dropdownClass.children[2].style.backgroundColor = "#2a2b2b";
         dropdownClass.children[2].style.color = "#ffffff";
@@ -19,10 +21,15 @@ export function applyDropdown(){
         dropdownClass.children[2].style.padding = "1rem";
         dropdownClass.children[2].style.opacity = "0";
         dropdownClass.children[2].style.height = "0";
+        dropdownClass.children[2].style.boxShadow = "0px 10px 50px rgba(0, 0, 0, 0.8)"
 
         Array.from(dropdownClass.children[2].children).forEach(child => {
             child.style.borderBottom = "2px solid gray";
-            child.style.borderRadius = "0.5rem";
+            child.style.borderRadius = "0.2rem";
+            child.style.marginBottom = "0.3rem";
+            child.style.marginRight = "0.5rem";
+            child.style.marginLeft = "0.5rem";
+            child.style.padding = "0.5rem";
         });
         dropdownClass.children[0].addEventListener("mouseover", () => {
             dropdownClass.children[2].style.opacity = "1"; // Make it visible
